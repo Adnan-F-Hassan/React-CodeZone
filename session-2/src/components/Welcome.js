9e95f2aa8d0a24787cfc2e17c9b03a2274843f2a
Adnan-F-Hassan <122805736+Adnan-F-Hassan@users.noreply.github.com>
2024-09-18 18:44:09 +0300

    useState - hook state

--- session-2/src/components/Welcome.js
+++ session-2/src/components/Welcome.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 function Welcome(props) {
+    let [counter, setCounter] = useState(0);
+
     console.log(props);
     return (
+        <>
         <h1>Hello , {props.name}</h1>
+        <h5> {counter}</h5>
+        <button
+                onClick = {() =>{
+                    setCounter(counter++);
+                }}
+            > counting
+            </button>
+        </>
     )
 }
 
