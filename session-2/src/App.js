18bbb67d533d5f202ae2543d8520f2f9af8ac325
Adnan-F-Hassan <122805736+Adnan-F-Hassan@users.noreply.github.com>
2024-09-18 23:29:57 +0300

    Form component

--- session-2/src/App.js
+++ session-2/src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button';
+import Form from './components/Form';
 import Welcome from "./components/Welcome";
 import WelcomeRender from './components/WelcomeRender';
 
@@ -8,9 +9,10 @@ import WelcomeRender from './components/WelcomeRender';
 function App() {
   return (
     <div className="App">
-    <Welcome name="Eden" age="27" title ="js dev" />
+    {/* <Welcome name="Eden" age="27" title ="js dev" />
     <WelcomeRender name="Edeno" />
-    <Button title="react-btn"/>
+    <Button title="react-btn"/> */}
+    <Form />
     </div>
   );
 }
