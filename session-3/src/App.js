c5bb9003e9296db0f63c7b29272d7a802620be88
Adnan-F-Hassan <122805736+Adnan-F-Hassan@users.noreply.github.com>
2024-09-19 01:00:14 +0300

    conditional  rendering

--- session-3/src/App.js
+++ session-3/src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import { useState } from "react";
+import Welcome from './components/Welcome';
+import Guest from './components/Guest';
 function App() {
   
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  let element = isLoggedIn ? <h1>Logged In ^_^ </h1> : <h2>not logged in yet!</h2>;
+  // let element = isLoggedIn ? <h1>Logged In ^_^ </h1> : <h2>not logged in yet!</h2>;
   return (
     <div className="App">
     <button onClick={() => setIsLoggedIn(true)}> Login </button>
-    {element}
-    
-    
+    {/* {element} */}
+    {/* {isLoggedIn ? <h1>Logged In ^_^ </h1> : <h2>not logged in yet!</h2>} */}
+    {isLoggedIn ? <Welcome name="Eden" /> :  <Guest />}
     
     
     
---
+++ session-3/src/components/Guest.js
@@ -0,0 +1,7 @@
+function Guest() {
+    return (
+        <h2> log in dude !! </h2>
+    )
+} 
+
+export default Guest;
