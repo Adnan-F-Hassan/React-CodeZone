6408051cf93e1c90c36d1145a7838e08fd6982e8
Adnan-F-Hassan <122805736+Adnan-F-Hassan@users.noreply.github.com>
2024-09-18 08:05:04 +0300

    components

--- session-2/src/App.js
+++ session-2/src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
+
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <div className="App">
-    <h1>Hello World</h1>
+    <Welcome name="Eden" age="27" title ="js dev" />
     </div>
   );
 }
