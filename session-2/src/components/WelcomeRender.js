ec974b0f420a192d905873e9a3b75d0c72211b8d
Adnan-F-Hassan <122805736+Adnan-F-Hassan@users.noreply.github.com>
2024-09-18 14:39:37 +0300

    state

--- session-2/src/components/WelcomeRender.js
+++ session-2/src/components/WelcomeRender.js
@@ -2,10 +2,17 @@ import React from "react";
 
 
 class WelcomeRender extends React.Component {
+    state = {
+        firstName: "Eden",
+        LastName: "Farid"
+    }
 
     render(){
         return (
+            <>
             <h1> Welcome Rendering Component, {this.props.name}</h1>
+            <h6> stated by: {this.state.firstName} {this.state.LastName}</h6>
+            </>
         )
     }
 }
