3b892a128eb3168dd2e53d6d669c1e209099ed30
Adnan-F-Hassan <122805736+Adnan-F-Hassan@users.noreply.github.com>
2024-09-23 18:26:19 +0300

    use Effect

--- session-4/src/Components/CounterFC.js
+++ session-4/src/Components/CounterFC.js
@@ -1,8 +1,13 @@
-import { useState } from "react";
+import { useState , useEffect} from "react";
 
 
 function CounterFc() {
     const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        document.title = `you clicked ${counter} times`; 
+    })
+
     return (
         <>
             <button onClick={() => setCounter(counter +1)} > + </button>
