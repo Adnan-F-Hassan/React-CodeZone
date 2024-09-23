6b629994b9da14996ea1af4c64c655b6ccd8decc
Adnan-F-Hassan <122805736+Adnan-F-Hassan@users.noreply.github.com>
2024-09-23 18:44:10 +0300

    CLEAN-UP - unmounting
    effect with cleanup

--- session-4/src/Components/CounterFC.js
+++ session-4/src/Components/CounterFC.js
@@ -5,7 +5,13 @@ function CounterFc() {
     const [counter, setCounter] = useState(0);
 
     useEffect(() => {
-        document.title = `you clicked ${counter} times`; 
+        // document.title = `you clicked ${counter} times`; 
+
+        return () => {
+            console.log("unmounting");
+            
+        }
+        
     }, []) // el array fel a5er da dependency list,. means that el 7aga elly bte3tmed 3leeha el useEffect 34an t-rerender  
 
     return (
