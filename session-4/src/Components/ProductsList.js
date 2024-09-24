c0aaf89bd2173557f1db5fb5b84c0d21cc10bbfd
Adnan-F-Hassan <122805736+Adnan-F-Hassan@users.noreply.github.com>
2024-09-24 16:10:14 +0300

    useEffect - fetch()

--- session-4/src/Components/ProductsList.js
+++ session-4/src/Components/ProductsList.js
@@ -1,16 +1,23 @@
 // import productsData from "../products-data.js";
+import { useState , useEffect} from "react";
 import Product from "./Product.js";
 import "./ProductsList.css";
 
 function ProductsList() {
 
-    const products = [].map((product) => {
-        return product.price < 100 && <Product key={product.id} product= {product}   />;
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products').then((response) => response.json()).then((data) => console.log(data))
         
-    });
+    })
+
+
+    // const products = [].map((product) => {
+    //     return product.price < 100 && <Product key={product.id} product= {product}   />;
+        
+    // });
 
     return  <div className = "products-List">
-            {products}
+            {/* {Product} */}
         </div>;
 }
 
