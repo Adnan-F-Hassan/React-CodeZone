9133087e9cf8f7bccab94835926f61d03dead7bf
Adnan-F-Hassan <122805736+Adnan-F-Hassan@users.noreply.github.com>
2024-09-27 00:08:20 +0300

    project

--- session-5/src/App.js
+++ session-5/src/App.js
@@ -1,11 +1,27 @@
-
+import Navbar from "./components/Navbar";
+import ProductsList from "./components/ProductsList";
+import Slider from "./components/Slider";
+import { Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import ProductDetails from "./components/ProductDetails";
 
 function App() {
   return (
     <div className="App">
-      <div className='container'>
-      <h1> Hello Bootstrap</h1>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Slider />
+              <ProductsList />
+            </>
+          }
+        />
+        <Route path="about" element={<About />} />
+        <Route path="product/:productId" element={<ProductDetails />} />
+      </Routes>
     </div>
   );
 }
