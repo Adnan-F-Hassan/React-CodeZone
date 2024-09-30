5a38970644882a61ffd5d21320ef93d257b4977c
Adnan-F-Hassan <122805736+Adnan-F-Hassan@users.noreply.github.com>
2024-09-30 18:09:05 +0300

    Pages

--- session-6/src/App.js
+++ session-6/src/App.js
@@ -1,11 +1,26 @@
+import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import Home from './Pages/Home';
+import Products from './Pages/Products';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-    <h1> HELLO WORLD</h1>
+      <div className='row'>
+        <div className='col-2 sidebar'>
+          <Sidebar />
+        </div>
+        <div className='col-10'>
+          <Routes>
+            <Route path = "/" element = {<Home />} />
+            {/* <Route path = "about" element = {<About />} /> */}
+            <Route path = "products" element = {<Products />} />
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ session-6/src/components/Sidebar.js
@@ -0,0 +1,18 @@
+// import { Link } from "react-router-dom";
+
+function Sidebar() {
+  return (
+    <>
+     <ul className="list-unstyled">
+      <li>
+        <a href = "#"> Get All Products </a>
+      </li>
+      <li>
+        <a href = "#"> Get All Categories </a>
+      </li>
+     </ul>
+    </>
+  )
+}
+
+export default Sidebar;
