56cad1749bf252dabdf56324ca588d7e67f6c33c
Adnan-F-Hassan <122805736+Adnan-F-Hassan@users.noreply.github.com>
2024-10-01 22:23:43 +0300

    view details page
    use params

--- session-6/src/Pages/ProductDetails.js
+++ session-6/src/Pages/ProductDetails.js
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function ProductDetails () {
      
+    const [product, setProduct] = useState();
+
+
+    let {productID} = useParams();
+
+    useEffect(() => {
+        fetch(`http://localhost:9000/products/${productID}`)
+        .then((res) => res.json())
+        .then((product) => {
+            setProduct(product)
+        });
+    }, []);
+
+
     return (
         <>
         
-            <h1>ProductDetails #</h1>
+            {product && <h1> {product.title} </h1> }
         
         </>
     ) 
