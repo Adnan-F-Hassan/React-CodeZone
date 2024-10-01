7df158f0e5194b2201e860216d226610a74ae8f9
Adnan-F-Hassan <122805736+Adnan-F-Hassan@users.noreply.github.com>
2024-10-02 00:02:50 +0300

    add product

--- session-6/src/Pages/AddProduct.js
+++ session-6/src/Pages/AddProduct.js
@@ -1,7 +1,74 @@
+import { useState } from "react";
+import axios from "axios";
+
 function AddProduct(){
+
+    const[title, setTitle] = useState('');
+    const[price, setPrice] = useState(0);
+
+    const formSubmit = (e) => {
+        e.preventDefault()
+
+        axios.post(`http://localhost:9000/products/${product.id}`, {
+                title, 
+                price
+            })
+            .then((data) => {console.log(data);
+            })
+
+        // fetch(`http://localhost:9000/products/${product.id}`, {
+        //     method:"POST",
+        //     body: JSON.stringify({
+        //         title, 
+        //         price
+        //     })
+        // })
+        // .then((res) => res.json())
+        // .then((data) => {
+        //     getAllProducts()
+        // })
+        
+    }
+
     return (
         <>
         <h1>Add Product</h1>
+
+        <form onSubmit={formSubmit}>
+            <div className="mb-3">
+                <label htmlFor="productTitle" className = "form-label">
+                    Title 
+                </label>
+                <input 
+                    type="text" 
+                    className="form-control" 
+                    id="productTitle" 
+                    placeholder="Product title"
+                    aria-describedby="Product title" 
+                    onChange={(e) => setTitle(e.target.value)}
+                    />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="productPrice" className = "form-label">
+                    Price 
+                </label>
+                <input 
+                    type="number" 
+                    className="form-control" 
+                    id="productPrice" 
+                    placeholder="Product Price"
+                    aria-describedby="product Price" 
+                    onChange={(e) => setPrice(e.target.value)}
+                />
+            </div>
+
+
+
+
+
+            <button type="submit" className="btn btn-primary">Add Product</button>
+
+        </form>
         </>
     )
 }
