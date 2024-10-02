b82cc9deb2254545941c3ebdcc053da826e78805
Adnan-F-Hassan <122805736+Adnan-F-Hassan@users.noreply.github.com>
2024-10-02 15:54:25 +0300

    components

--- session-7/src/App.js
+++ session-7/src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState } from "react";
+import Navbar from './components/Navbar';
+import ProductsList from './components/ProductsList';
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [products, setProduct] = useState([
+    {id: 1, title: "prod 1" },
+    {id: 2, title: "prod 2" },
+    {id: 3, title: "prod 3" }
+  ]);
+
+
+
+  return <div className="App">
+      
+    <Navbar count={products.length} />
+    <ProductsList products={products} />
+
     </div>
-  );
 }
 
 export default App;
---
+++ session-7/src/components/ProductsList.js
@@ -0,0 +1,10 @@
+
+function ProductsList(props) {
+  return (
+    <div className="products-list">
+      {props.products.map((product) => <h1 key={product.id}>{product.title}</h1>)} 
+    </div>
+  );
+}
+
+export default ProductsList;
