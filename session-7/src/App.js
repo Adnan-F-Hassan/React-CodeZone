5e5e8845ec339c989d063bf5c15b3cacc45f3882
Adnan-F-Hassan <122805736+Adnan-F-Hassan@users.noreply.github.com>
2024-10-12 16:29:54 +0300

    Update App.js

--- session-7/src/App.js
+++ session-7/src/App.js
@@ -1,28 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { createContext, useState } from "react";
+import { useState } from "react";
 import Navbar from './components/Navbar';
 import ProductsList from './components/ProductsList';
 
-export const ProductsContext = createContext() //   () by default undefined ... (null)
 
 function App() {
 
-  const [products, setProduct] = useState([
-    {id: 1, title: "prod 1" },
-    {id: 2, title: "prod 2" },
-    {id: 3, title: "prod 3" }
-  ]);
-
-
-
     return (
       <div className="App">
-        <ProductsContext.Provider value={products}>
           <Navbar />
-          <ProductsList products={products} />
-        </ProductsContext.Provider>
+          <ProductsList />
       </div>
     )
 }
